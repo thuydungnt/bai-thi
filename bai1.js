7d656ec7744714957754cec79cea3db2b13b8be3
thuydungnt <thuydungnt96@gmail.com>
2023-04-12 22:31:16 +0700

    push

---
+++ bai1.js
@@ -0,0 +1,10 @@
+// console.log(10)
+function numberOneTriangle(b){
+    for(let i=0; i <= b; i++){
+        console.log(i)
+        for(let j = i; j < i -1; j++){
+        console.log(j)
+        }
+    }
+}
+numberOneTriangle(10)
